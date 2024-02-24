fd350a47cd911f0c57e8e269241a25053ea7a251
ali-murtaxa <xd_ali@live.com>
2024-02-24 17:31:00 +0500

    register page

--- mycrm/urls.py
+++ mycrm/urls.py
@@ -7,4 +7,5 @@ from mycrm import views
 urlpatterns = [
     path('', views.home,name='home'),
     path('logout', views.logout_user,name='logout'),
+    path('Register', views.Register_user,name='Register'),
 ]
