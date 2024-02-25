50b9e34dc22b6ce9ad1892dbfa5bfc352744255e
ali-murtaxa <xd_ali@live.com>
2024-02-25 16:44:30 +0500

    worked on registeration form

--- mycrm/forms.py
+++ mycrm/forms.py
@@ -3,9 +3,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
 class SignUpForm(UserCreationForm):
-    Name=forms.CharField(label='Name',widget=forms.PasswordInput(attrs={'class' : 'form-control'}))
-    email=forms.CharField(label='Email Address',widget=forms.PasswordInput(attrs={'class' : 'form-control'}))
-    username=forms.CharField(label='Username',widget=forms.PasswordInput(attrs={'class' : 'form-control'}))
+    Name=forms.CharField(label='Name',widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    email=forms.CharField(label='Email Address',widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    username=forms.CharField(label='Username',widget=forms.TextInput(attrs={'class' : 'form-control'}))
     password1=forms.CharField(label='Password',widget=forms.PasswordInput(attrs={'class' : 'form-control'}))
     password2=forms.CharField(label='Password Confirm',widget=forms.PasswordInput(attrs={'class' : 'form-control'}))
     class Meta:
